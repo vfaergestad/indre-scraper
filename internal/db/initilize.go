03dda6e97c03b67d18d1549b9c28dbcf57069c5a
Vegard Færgestad <vegarfae@stud.ntnu.no>
2022-04-12 23:08:29 +0200

    Added functionality for saving tags to firestore

---
+++ internal/db/initilize.go
@@ -0,0 +1,51 @@
+package db
+
+import (
+	"cloud.google.com/go/firestore"
+	"context"
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+)
+
+// Firebase context and client used by Firestore functions throughout the program.
+var ctx context.Context
+var client *firestore.Client
+
+// InitializeFirestore Sets the context and client for Firestore.
+func InitializeFirestore() error {
+	ctx = context.Background()
+
+	// Creates the Firebase App using the service account credentials.
+	sa := option.WithCredentialsFile("serviceAccountKey.json")
+	app, err := firebase.NewApp(ctx, nil, sa)
+	if err != nil {
+		return err
+	}
+
+	client, err = app.Firestore(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+// CloseFirestore Closes the Firestore client.
+func CloseFirestore() error {
+	err := client.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetContext Returns the Firestore context.
+func GetContext() context.Context {
+	return ctx
+}
+
+// GetClient Returns the Firestore client.
+func GetClient() *firestore.Client {
+	return client
+}
---
+++ internal/summaries/util/sort_tags.go
@@ -0,0 +1,93 @@
+package util
+
+// SortTags sorts the tags by their count
+func SortTags(tags map[string]int) []string {
+	var keys []string
+
+	for key := range tags {
+		keys = append(keys, key)
+	}
+
+	for i := 0; i < len(keys); i++ {
+		for j := 0; j < len(keys)-i-1; j++ {
+			if tags[keys[j]] < tags[keys[j+1]] {
+				keys[j], keys[j+1] = keys[j+1], keys[j]
+			}
+		}
+	}
+
+	return keys
+}
+
+var locations = []string{
+	"Aurskog",
+	"Aurskog-Høland",
+	"Aursmoen",
+	"Bjørkelangen",
+	"Blaker",
+	"Fet",
+	"Fetsund",
+	"Fosser",
+	"Gan",
+	"Lierfoss",
+	"Lillestrøm",
+	"Løken",
+	"Lørenfallet",
+	"Momoen",
+	"Mork",
+	"Rømskog",
+	"Setskog",
+	"Søndre Mangen",
+	"Sørumsand",
+	"Ukraina",
+}
+
+func IsValidLocation(a string) bool {
+	for _, b := range locations {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+var aHLocations = []string{
+	"Aurskog",
+	"Aurskog-Høland",
+	"Aursmoen",
+	"Bjørkelangen",
+	"Fosser",
+	"Gan",
+	"Lierfoss",
+	"Løken",
+	"Momoen",
+	"Rømskog",
+	"Setskog",
+	"Søndre Mangen",
+}
+
+func AmountInAH(locations map[string]int) int {
+	count := 0
+	for _, location := range aHLocations {
+		count += locations[location]
+	}
+	return count
+}
+
+var lsLocations = []string{
+	"Blaker",
+	"Fet",
+	"Fetsund",
+	"Lillestrøm",
+	"Lørenfallet",
+	"Mork",
+	"Sørumsand",
+}
+
+func AmountInLS(locations map[string]int) int {
+	count := 0
+	for _, location := range lsLocations {
+		count += locations[location]
+	}
+	return count
+}
