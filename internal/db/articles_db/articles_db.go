b7d44442fe34a2c96e4bce39dae9e0f66b069959
vegarfae <vegarfae@stud.ntnu.no>
2022-04-17 21:01:17 +0200

    Added database for articles

---
+++ internal/db/articles_db/articles_db.go
@@ -0,0 +1,60 @@
+package articles_db
+
+import (
+	"google.golang.org/api/iterator"
+	"indre-scraper/internal/db"
+	"indre-scraper/internal/structs"
+	"strings"
+	"time"
+)
+
+const collection = "articles"
+
+func AddArticle(article structs.Article) error {
+	_, err := db.GetClient().Collection(collection).Doc(getIDFromLink(article.Link)).Set(db.GetContext(), article)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetArticle(link string) (structs.Article, error) {
+	article := structs.Article{}
+	doc, err := db.GetClient().Collection(collection).Doc(getIDFromLink(link)).Get(db.GetContext())
+	if err != nil {
+		return structs.Article{}, err
+	}
+	err = doc.DataTo(&article)
+	if err != nil {
+		return structs.Article{}, err
+	}
+	return article, nil
+}
+
+func GetArticlesFromRange(startDate time.Time, endDate time.Time) ([]structs.Article, error) {
+	var articles []structs.Article
+	query := db.GetClient().Collection(collection).Where("published", ">=", startDate).Where("date", "<=", endDate)
+	iter := query.Documents(db.GetContext())
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		var article structs.Article
+		err = doc.DataTo(&article)
+		if err != nil {
+			return nil, err
+		}
+		articles = append(articles, article)
+	}
+
+	return articles, nil
+}
+
+func getIDFromLink(link string) string {
+	splitLink := strings.Split(link, "/")
+	return splitLink[len(splitLink)-1]
+}
